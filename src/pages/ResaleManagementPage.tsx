3c59f9f6b4291d890c7929a05a3ed48b378e2c4c
reportyao <22315113+reportyao@users.noreply.github.com>
2025-12-24 10:08:41 -0500

    fix: 修复ResaleManagementPage语法错误

--- src/pages/ResaleManagementPage.tsx
+++ src/pages/ResaleManagementPage.tsx
@@ -422,7 +422,7 @@ export default function ResaleManagementPage() {
                     </button>
                   </td>
                 </tr>
-              )}))
+              )})
             )}
           </tbody>
         </table>
